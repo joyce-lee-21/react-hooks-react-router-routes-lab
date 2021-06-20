f5754593a498d10bee9a98030be6b60eb17ccaa0
Joyce Lee <joycelee901@gmail.com>
2021-06-20 17:46:34 -0500

    commit tree 66daffb8068506ab94cea33ba0ee0b42e509d864

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,56 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <ul>
+        <li>
+          <NavLink
+            to="/"
+            exact
+            activeStyle={{
+              color: "darkblue",
+            }}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/movies"
+            exact
+            activeStyle={{
+              color: "darkblue",
+            }}
+          >
+            Movies
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/directors"
+            exact
+            activeStyle={{
+              color: "darkblue",
+            }}
+          >
+            Directors
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/actors"
+            exact
+            activeStyle={{
+              color: "darkblue",
+            }}
+          >
+            Actors
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default NavBar;
